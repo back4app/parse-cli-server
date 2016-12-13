b4844f56efadce35fc5526bfd22020df51bc4f72
Luis Holanda <luiscmholanda@gmail.com>
2016-12-13 17:56:34 -0200

    Fixed large files upload problems.
    	The default express limit for HTTP requests lengths is very low.
    Added body-parser to increase this limit. Default set to 50Mb.

--- src/ParseCliServer.js
+++ src/ParseCliServer.js
@@ -34,8 +34,13 @@ class ParseCliServer {
     parse-cli always send json body but don't send a Content-Type
     header or in some cases send an unexpected value like
     application/octet-stream.
+    express request length limit is very low. Change limit value
+    for fix 'big' files deploy problems.
     */
-    app.use(bodyParser.json({type: function() { return true; }}));
+    let limit = '50mb';
+    app.use(bodyParser.json({type: function() { return true; }, limit: limit}));
+    app.use(bodyParser.urlencoded({limit: limit, extend: true}));
+
     this.router.mountOnto(app);
     return app;
   }
