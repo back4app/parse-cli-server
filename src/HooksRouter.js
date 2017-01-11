b21e8d6c2a875ddd91a13524b4f10e019ca2d1c2
Luis Holanda <luiscmholanda@gmail.com>
2017-01-11 17:44:26 -0200

    Changed error handling in AccountKey authorization (#13)
    *  Changed error handling in promiseEnforceAccountKeyAccess

--- src/HooksRouter.js
+++ src/HooksRouter.js
@@ -1,31 +1,31 @@
 import { HooksRouter as _HooksRouter } from 'parse-server/lib/Routers/HooksRouter';
-import * as middlewares from "parse-server/lib/middlewares";
+import * as middlewares from 'parse-server/lib/middlewares';
 const express = require('express');
 
 export default class HooksRouter extends _HooksRouter {
 
-    handleGetFunctions(req) {
-        return super.handleGetFunctions(req)
-        .then(this._patchResponse);
-    }
+  handleGetFunctions(req) {
+    return super.handleGetFunctions(req)
+      .then(this._patchResponse);
+  }
 
-    handleGetTriggers(req) {
-        return super.handleGetTriggers(req)
-        .then(this._patchResponse);
-    }
+  handleGetTriggers(req) {
+    return super.handleGetTriggers(req)
+      .then(this._patchResponse);
+  }
 
-    _patchResponse(response) {
-        return {
-            response: {
-                results: response.response,
-            }
-        };
-    }
+  _patchResponse(response) {
+    return {
+      response: {
+        results: response.response,
+      }
+    };
+  }
 
-    mountOnto(app) {
-        var hooksApp = express();
-        hooksApp.use('/hooks', middlewares.handleParseHeaders);
-        super.mountOnto(hooksApp);
-        return app.use('/1', hooksApp);
-    }
+  mountOnto(app) {
+    var hooksApp = express();
+    hooksApp.use('/hooks', middlewares.handleParseHeaders);
+    super.mountOnto(hooksApp);
+    return app.use('/1', hooksApp);
+  }
 }
